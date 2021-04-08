16610846e44634d48b8bc143d47c7ca8519ddfab
pedrosanmi23 <pedrosanmi23@gmail.com>
2021-04-08 15:36:42 +0200

    Funcionalidad3

--- 1y3.sh
+++ 1y3.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+#Funcionalidad 3 - Comprobar la existencia de un grupo
+
+read -p "Introduce el nombre de un grupo: " grupo
+
+if grep -q $grupo /etc/group
+	then
+		echo "El grupo $grupo existe"
+	else
+		echo "El grupo $grupo no existe"
+	fi
+
+	
+
